bd7bd6781fb1896df1f328a7db661691b01b1db1
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2025-12-30 09:40:34 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import Images from './components/Images';
 
 const App = () => {
 
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(100)
+
+  const getData = async () => {
+    const response = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=8`);
+    const data = await response.json();
+    setImages(data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [page])
+
   return (
     <div className='min-h-screen w-screen bg-gray-900 text-white flex flex-col items-center justify-start p-6'>
-      <div className="title font-bold text-2xl text-center text-gray-300 mb-6">Gallery</div>
-
-      <div className="images flex flex-wrap justify-center gap-4 bg-[#535660] p-6 rounded-lg max-w-6xl">
-        <a href="" className='flex flex-col items-center bg-gray-800 p-4 rounded-lg hover:scale-105 duration-300 transition-transform'>
-          <div className="image w-58 h-49 rounded-lg overflow-hidden flex items-center justify-center mb-2">
-            <img src="https://picsum.photos/200" alt="Random" className='w-full h-full object-cover' />
-          </div>
-          <h3 className='font-semibold text-center'>Abdul Kareem</h3>
-        </a>
+      <div className="title font-bold text-2xl text-center text-gray-300 mb-6">
+        Gallery
       </div>
+
+      <Images images={images} />
 
       <div className="btns flex items-center gap-4 justify-center mt-6">
-        <button className='px-4 py-2 bg-gray-800 rounded-lg hover:bg-gray-700 duration-300 transition-colors'>prev</button>
-        <div className="page-no font-bold text-blue-500">8</div>
-        <button className='px-4 py-2 bg-gray-800 rounded-lg hover:bg-gray-700 duration-300 transition-colors'>next</button>
-      </div>
+        <button
+          onClick={() => { if (page !== 100) setPage(page - 1) }}
+          style={{ opacity: page === 100 ? 0.5 : 1, cursor: page === 100 ? 'not-allowed' : 'default' }}
+          className='px-4 py-2 bg-gray-800 rounded-lg hover:bg-gray-700 duration-300 transition-colors'>
+          prev
+        </button>
 
+        <div className="page-no font-bold text-blue-500">
+          {page - 99}
+        </div>
+
+        <button
+          onClick={() => setPage(page + 1)}
+          className='px-4 py-2 bg-gray-800 rounded-lg hover:bg-gray-700 duration-300 transition-colors cursor-pointer'>
+          next
+        </button>
+      </div>
     </div>
   )
 }
---
+++ src/components/Images.jsx
@@ -0,0 +1,23 @@
+const Images = ({ images }) => {
+    return (
+        <div className="images flex flex-wrap justify-center gap-4 bg-[#535660] p-6 rounded-lg max-w-6xl">
+            {images.map((img, idx) => {
+                return <a
+                    href={img.url}
+                    key={idx}
+                    target='_blank'
+                    className='flex flex-col items-center bg-gray-800 p-4 rounded-lg hover:scale-105 duration-300 transition-transform'>
+                    <div className="image w-58 h-49 rounded-lg overflow-hidden flex items-center justify-center mb-2">
+                        <img
+                            src={img.download_url}
+                            alt="Random"
+                            className='w-full h-full object-cover' />
+                    </div>
+                    <h3 className='font-semibold text-center'>{img.author}</h3>
+                </a>
+            })}
+        </div>
+    )
+}
+
+export default Images
